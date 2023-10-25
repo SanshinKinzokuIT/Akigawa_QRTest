ee27e4f6641531166e8604ec75fe3c9e117b0b4c
SANSHIN\phamngoc_an <andiem171994@gmail.com>
2023-10-25 16:41:51 +0900

    first commit

---
+++ QRTest/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace QRTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("ＭＳ ゴシック", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(166, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 48);
+            this.label2.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 233);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ QRTest/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using WindowsInput;
+
+namespace QRTest
+{
+    public partial class Form1 : Form
+    {
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        // Lưu trữ tham chiếu đến Form1
+        private static Form1 currentForm;
+        public Form1()
+        {
+            InitializeComponent();
+            _hookID = SetHook(_proc);
+            currentForm = this;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private struct WinText
+        {
+            public IntPtr hWnd;
+            public string Text;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+        }
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                char keyPressed = (char)vkCode;
+
+                // Gọi hàm UpdateLabel2 và truyền giá trị newText
+                if (currentForm != null)
+                {
+                    currentForm.Invoke(new Action(() => UpdateLabel2(keyPressed.ToString())));
+                }
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+        private char GetLastChar(string input)
+        {
+            if (!string.IsNullOrEmpty(input))
+            {
+                return input[input.Length - 1];
+            }
+
+            // Trả về một giá trị mặc định hoặc xử lý khác tùy thuộc vào yêu cầu của bạn.
+            return '\0';
+        }
+        private static void UpdateLabel2(string newText)
+        {
+
+            // Sử dụng label2 trực tiếp từ ActiveForm
+            if (currentForm != null)
+            {
+                Label label2 = currentForm.Controls["label2"] as Label;
+                if (label2 != null)
+                {
+                    label2.Text = newText;
+                }
+            }
+        }
+    }
+}
